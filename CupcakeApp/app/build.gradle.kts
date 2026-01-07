c86d74dd3a8cdd4f6ff54101df5db72a10655127
Pranshu.Katiyar <pranshukatiyar3957@gmail.com>
2026-01-07 15:54:45 +0530

    Update Cupcake app logic and UI

--- CupcakeApp/app/build.gradle.kts
+++ CupcakeApp/app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.7.0")
 
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
@@ -61,13 +61,12 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    // ✅ Navigation (stable)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    // ✅ Lifecycle compose
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
     testImplementation("junit:junit:4.13.2")
-
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }

daf057ddabd19ae0277df1087e9cb058a662cd4e
Pranshu.Katiyar <pranshukatiyar3957@gmail.com>
2026-01-14 13:08:46 +0530

    Pratice the adaptive layout by building sport app

---
+++ Sports/app/src/main/java/com/example/sports/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.sports
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import com.example.sports.ui.SportsApp
+import com.example.sports.ui.theme.SportsTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            SportsTheme {
+                Surface {
+                    val windowSize = calculateWindowSizeClass(activity = this)
+                    SportsApp(
+                        windowSize = windowSize.widthSizeClass
+                    )
+                }
+            }
+        }
+    }
+}

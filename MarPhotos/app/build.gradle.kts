57d7c768fd08102f633f48eb531fed41118f805d
Pranshu.Katiyar <pranshukatiyar3957@gmail.com>
2026-01-16 16:13:49 +0530

    Mars App Practice: Test with fake data and repository

--- MarPhotos/app/build.gradle.kts
+++ MarPhotos/app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
